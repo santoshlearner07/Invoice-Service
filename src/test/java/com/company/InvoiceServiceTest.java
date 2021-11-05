0d311156a4f07bb79e995c94848bf38e0551519c
santoshlearner07 <nandiyawarsantosh.0719@gmail.com>
2021-11-05 19:29:26 +0530

    Fare Calculate from dist and time

--- src/test/java/com/company/InvoiceServiceTest.java
+++ src/test/java/com/company/InvoiceServiceTest.java
@@ -5,6 +5,17 @@ import org.junit.Test;
 
 public class InvoiceServiceTest {
 
+    @Test
+    public void givenDistAndTime_ShouldReturnTotalFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance = 2.0;
+        int time = 5;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(25, fare, 0.0);
+
+    }
+
+
 
     public static void main(String[] args) {
         System.out.println("Welcome to Invoice Service Program");
